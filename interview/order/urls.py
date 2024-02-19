08a06f1e91e986c5057cf8e9213c490fc7cff20b
Sergio Infante <raulsergio9@gmail.com>
2024-02-19 14:32:39 -0500

    Write an endpoint that lists orders that are between a particular start and embargo date.

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
 
+from interview.order.views import (OrderListCreateView, OrdersBetweenDatesView,
+                                   OrderTagListCreateView)
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('between_dates/<str:start_date>/<str:embargo_date>/', OrdersBetweenDatesView.as_view(), name='orders-between-dates')
 ]
